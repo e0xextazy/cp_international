2da3c222cb2bcca940b7c44d1c72beef267fb313
Danil Kladnitsky <danyakladnitsky@gmail.com>
2023-11-24 17:31:40 +0300

    frontend: add features

--- frontend/src/pages/ActionPage/sub/SendRequestForm/SendRequestForm.tsx
+++ frontend/src/pages/ActionPage/sub/SendRequestForm/SendRequestForm.tsx
@@ -1,4 +1,4 @@
-import { Textarea, TextareaProps } from '@mantine/core';
+import { Fieldset, Textarea, TextareaProps } from '@mantine/core';
 
 import { useAppStore } from '../../../../store';
 import { ActionPageFormProps } from '../../../../shared';
@@ -6,8 +6,8 @@ import { ActionPageFormProps } from '../../../../shared';
 import styles from './SendRequestForm.module.scss';
 
 const TEXTAREA_CONFIG: TextareaProps = {
-  minRows: 6,
-  maxRows: 12,
+  minRows: 8,
+  maxRows: 8,
   autosize: true,
 };
 
@@ -17,15 +17,17 @@ export const SendRequestForm = ({ className }: ActionPageFormProps) => {
   return (
     <div className={className}>
       <h2 className="title">Заполните обращение</h2>
-      <Textarea
-        onChange={(e) => setRequest(e.target.value)}
-        value={request}
-        variant="filled"
-        label="Введите обращение гражданина"
-        placeholder="Обращение"
-        className={styles.textarea}
-        {...TEXTAREA_CONFIG}
-      />
+      <Fieldset>
+        <Textarea
+          variant="filled"
+          onChange={(e) => setRequest(e.target.value)}
+          value={request}
+          label="Введите обращение гражданина"
+          placeholder="Обращение"
+          className={styles.textarea}
+          {...TEXTAREA_CONFIG}
+        />
+      </Fieldset>
     </div>
   );
 };
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -5,6 +5,8 @@ export interface Store {
   page: AppPages;
   request: string | null;
   response: any | null;
+  confidenceThreshold: number;
+  setConfidenceThreshold: (threshold: number) => void;
   setRequest: (request: string) => void;
   setResponse: (response: any) => void;
   setPage: (page: AppPages) => void;
@@ -14,7 +16,9 @@ export interface Store {
 const useAppStore = create<Store>((set) => ({
   request: null,
   response: null,
+  confidenceThreshold: 95,
   page: AppPages.START_PAGE,
+  setConfidenceThreshold: (threshold: number) => set(() => ({ confidenceThreshold: threshold })),
   setRequest: (request: string) => set(() => ({ request })),
   setResponse: (response: any) => set(() => ({ response })),
   setPage: (page: AppPages) => set(() => ({ page })),
