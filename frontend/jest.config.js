abf5544a74fba61399f3dcaed2226e15130121d0
Danil Kladnitsky <danyakladnitsky@gmail.com>
2023-11-24 17:58:16 +0300

    frontend: add tests

---
+++ frontend/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
---
+++ frontend/src/store/__tests__/useAppStore.test.ts
@@ -0,0 +1,55 @@
+import { renderHook, act } from '@testing-library/react';
+import { useAppStore } from '../index';
+import { AppPages } from '../../app/config';
+
+describe('useAppStore', () => {
+  test('should set confidence threshold', () => {
+    const { result } = renderHook(() => useAppStore());
+
+    act(() => {
+      result.current.setConfidenceThreshold(80);
+    });
+
+    expect(result.current.confidenceThreshold).toBe(80);
+  });
+
+  test('should set request', () => {
+    const { result } = renderHook(() => useAppStore());
+
+    act(() => {
+      result.current.setRequest('sample request');
+    });
+
+    expect(result.current.request).toBe('sample request');
+  });
+
+  test('should set response', () => {
+    const { result } = renderHook(() => useAppStore());
+
+    act(() => {
+      result.current.setResponse('sample response');
+    });
+
+    expect(result.current.response).toBe('sample response');
+  });
+
+  test('should set page', () => {
+    const { result } = renderHook(() => useAppStore());
+
+    act(() => {
+      result.current.setPage(AppPages.ACTION_PAGE);
+    });
+
+    expect(result.current.page).toBe(AppPages.ACTION_PAGE);
+  });
+
+  test('should go to home', () => {
+    const { result } = renderHook(() => useAppStore());
+
+    act(() => {
+      result.current.goToHome();
+    });
+
+    expect(result.current.page).toBe(AppPages.START_PAGE);
+  });
+});
